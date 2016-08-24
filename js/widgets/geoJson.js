81e79fc7c918c19c5a888c44e1eaed9678a267e3
pr!0ri7y <jai.priority@live.com>
2016-08-24 13:07:57 +0530

    Update geoJson.js

--- js/widgets/geoJson.js
+++ js/widgets/geoJson.js
@@ -1,1 +1,23 @@
 
+var geoJson = Class.create({
+
+    init: function (map) {
+        this.map = map;
+        this.initialize();
+    },
+    initialize: function () {
+       
+       this.st1 =  new google.maps.Data();
+       this.st2 =  new google.maps.Data();
+       this.st3 =  new google.maps.Data();
+       
+       st1.loadGeoJson('http://pri0ri7y.github.io/GMAPS100/assets/state.json');
+       st2.loadGeoJson('http://pri0ri7y.github.io/GMAPS100/assets/state.json');
+       st3.loadGeoJson('http://pri0ri7y.github.io/GMAPS100/assets/state.json');
+       
+    },
+    exportOnclick:fuction(e){
+      console.log(e);      
+    }
+    
+});
